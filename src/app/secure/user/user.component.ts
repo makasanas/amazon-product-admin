ed88941e69a394d1492ecd356611ac0dc207d810
Sanjay <makasanas@yahoo.in>
2019-10-03 23:28:10 +0530

    first commit

---
+++ src/app/secure/user/user.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "./user.service";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+
+  
+
+  public users:any = [];
+  public page = {
+		count: 0,
+		limit: 0,
+		offset: 0,
+		pageSize: 0
+  }
+  
+  constructor(private userService: UserService) { 
+    this.page.offset = 0;
+		this.page.limit = 10;
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.userService.getUsers(1,10).subscribe((res) => {
+      console.log(res.data);
+      this.users = res.data.users;
+      console.log(this.users);
+      this.page.count = res.data.count;
+    }, err => {
+    });
+  }
+
+  getAccess(row){
+
+    this.userService.getAccessToken(row.shopUrl).subscribe((res) => {
+      // console.log(res.data);
+      // this.users = res.data.users;
+      // console.log(this.users);
+      // this.page.count = res.data.count;
+      console.log(res.data);
+      console.log(window.location);
+      let url = 'http://localhost:4200/app/auth?token='+res.data.token;
+      console.log(url);
+       window.open(url, '_blank');
+    }, err => {
+    });
+   
+  }
+}
---
+++ src/app/secure/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Http, Headers, Response } from '@angular/http';
+import { environment } from './../../../environments/environment';
+
+
+@Injectable()
+export class UserService {
+
+    constructor(private http: Http) { }
+
+    createAuthorizationHeader(headers: Headers) {
+        headers.append('Authorization', localStorage.getItem('token').replace(/\"/g, ""));
+    }
+
+    getUsers(page, limit) {
+        let headers = new Headers();
+        this.createAuthorizationHeader(headers);
+        return this.http.get(environment.apiUrl + 'admin/user?page='+page+'&limit='+limit, { headers: headers }).pipe(map((response: any) => response.json()));
+    }
+
+    getAccessToken(shopUrl) {
+        let headers = new Headers();
+        this.createAuthorizationHeader(headers);
+        return this.http.get(environment.apiUrl + 'admin/access_token?shopUrl='+shopUrl, { headers: headers }).pipe(map((response: any) => response.json()));
+    }
+}
