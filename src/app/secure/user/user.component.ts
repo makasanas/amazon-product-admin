789b38176519cebe1713134d40a6578d018ebfa7
Sanjay <makasanas@yahoo.in>
2019-10-04 12:43:49 +0530

    user table updated

--- src/app/secure/user/user.component.ts
+++ src/app/secure/user/user.component.ts
@@ -9,45 +9,47 @@ import { environment } from './../../../environments/environment';
 })
 export class UserComponent implements OnInit {
 
-  
 
-  public users:any = [];
+
+  public users: any = [];
   public page = {
-		count: 0,
-		limit: 0,
-		offset: 0,
-		pageSize: 0
+    count: 0,
+    limit: 0,
+    offset: 0,
+    pageSize: 0
   }
-  
-  constructor(private userService: UserService) { 
+
+  constructor(private userService: UserService) {
     this.page.offset = 0;
-		this.page.limit = 10;
+    this.page.limit = localStorage.getItem('pageLimit') ? parseInt(localStorage.getItem('pageLimit')) : 10;
   }
 
   ngOnInit() {
-    this.getUsers();
+    this.getUsers(this.page);
   }
 
-  getUsers(){
-    this.userService.getUsers(1,10).subscribe((res) => {
-      console.log(res.data);
+  getUsers(page) {
+    this.userService.getUsers(page.offset + 1, page.limit).subscribe((res) => {
       this.users = res.data.users;
-      console.log(this.users);
       this.page.count = res.data.count;
     }, err => {
     });
   }
 
-  getAccess(row){
-
+  getAccess(row) {
     this.userService.getAccessToken(row.shopUrl).subscribe((res) => {
       console.log(res.data);
       console.log(window.location);
-      let url = environment.appUrl+'/app/auth?token='+res.data.token;
+      let url = environment.appUrl + '/app/auth?token=' + res.data.token;
       console.log(url);
-       window.open(url, '_blank');
+      window.open(url, '_blank');
     }, err => {
     });
-   
   }
+
+
+  pageLimit() {
+		this.getUsers(this.page);
+		localStorage.setItem('pageLimit', this.page.limit.toString());
+	}
 }
