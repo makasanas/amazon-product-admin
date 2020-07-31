23f48bfdc71d3768a6d0ce612d9169bed191e81d
Sanjay <makasanas@yahoo.in>
2020-07-31 11:45:18 +0530

    all product updated

--- src/app/secure/deleted-user/deleted-user.component.ts
+++ src/app/secure/deleted-user/deleted-user.component.ts
@@ -1,108 +1,197 @@
-import { Component, OnInit } from '@angular/core';
-import { DeletedUserService } from './deleted-user.service';
+import { Component, OnInit } from "@angular/core";
+import { DeletedUserService } from "./deleted-user.service";
 import { UserService } from "././../user/user.service";
-import { countryData } from './../../country.data';
-import { FormGroup, FormBuilder, FormControl, FormArray, Validators, FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-
-
-
+import { countryData } from "./../../country.data";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  FormArray,
+  Validators,
+  FormsModule,
+} from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-deleted-user',
-  templateUrl: './deleted-user.component.html',
-  styleUrls: ['./deleted-user.component.scss']
+  selector: "app-deleted-user",
+  templateUrl: "./deleted-user.component.html",
+  styleUrls: ["./deleted-user.component.scss"],
 })
 export class DeletedUserComponent implements OnInit {
-
   public users: any = [];
   public page = {
     count: 0,
     limit: 0,
     offset: 0,
-    pageSize: 0
-  }
+    pageSize: 0,
+  };
   public loading = false;
 
+  public deleteCatogaryArray = [];
 
-  constructor(private Router : Router, private userService: UserService , private deletedUserService: DeletedUserService, private FormBuilder : FormBuilder) {
-
+  constructor(
+    private Router: Router,
+    private userService: UserService,
+    private deletedUserService: DeletedUserService,
+    private FormBuilder: FormBuilder
+  ) {
     this.countryForm = this.FormBuilder.group({
-      countryValue : {}
+      countryValue: {},
     });
 
     this.page.offset = 0;
-    this.page.limit = localStorage.getItem('pageLimit') ? parseInt(localStorage.getItem('pageLimit')) : 10;
+    this.page.limit = localStorage.getItem("pageLimit")
+      ? parseInt(localStorage.getItem("pageLimit"))
+      : 10;
   }
 
-  public countryData = countryData ;
-  public countryForm : any ; 
-  public categoryData :any ;
-
-
+  public countryData = countryData;
+  public countryForm: any;
+  public categoryData: any;
 
   ngOnInit() {
-    // this.getUsers(this.page);
-    // console.log(this.users);
     this.getCategoryData(this.page);
-
   }
 
-
   getCategoryData(page) {
-    this.userService.getCategoryData(page.offset + 1, page.limit).subscribe((res)=> {
-      console.log(res)
-      this.categoryData = res.data.category;
-      this.page.count = res.data.count;
-      this.page = page;
-      this.page.count = res.data.count;
-    })
+    this.userService
+      .getCategoryData(page.offset + 1, page.limit)
+      .subscribe((res) => {
+        console.log(res);
+        this.categoryData = res.data.category;
+        this.page.count = res.data.count;
+        this.page = page;
+        this.page.count = res.data.count;
+      });
   }
 
   updateCountry(value) {
     let Data = {
-      "title" : value.document.title , 
-      "country" : {
-        "name" : this.countryForm.value.countryValue.itemName , 
-        "code" : this.countryForm.value.countryValue.itemCode 
-      }
-    }
+      title: value.document.title,
+      country: {
+        name: this.countryForm.value.countryValue.itemName,
+        code: this.countryForm.value.countryValue.itemCode,
+      },
+    };
 
-    this.userService.updateData(Data).subscribe((res)=>{
-    this.getCategoryData(this.page);
-    this.countryForm.reset()
-    }),error  => {
-    }
+    this.userService.updateData(Data).subscribe((res) => {
+      this.getCategoryData(this.page);
+      this.countryForm.reset();
+    }),
+      (error) => {};
   }
 
   getUsers(page) {
     this.loading = true;
-    this.deletedUserService.getUsers(page.offset + 1, page.limit).subscribe((res) => {
-      this.users = res.data.deletedUser;
-      console.log(this.users);
-      this.page = page;
-      this.page.count = res.data.count;
-      this.loading = false;
+    this.deletedUserService.getUsers(page.offset + 1, page.limit).subscribe(
+      (res) => {
+        this.users = res.data.deletedUser;
+        console.log(this.users);
+        this.page = page;
+        this.page.count = res.data.count;
+        this.loading = false;
 
-      
-      this.users.forEach(user => {
-        var today = new Date(new Date().toJSON().slice(0, 10));
-        let diffTime = Math.abs(today.getTime() - new Date(user.trial_start).getTime());
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-        user.trial_days = user.trial_days - diffDays > 0 ? user.trial_days - diffDays : 0;
-        let dt1 = new Date(user.created);
-        let dt2 = new Date(user.updated);
-        user.appUsed = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
-      });
-    }, err => {
-      this.loading = false;
-    });
+        this.users.forEach((user) => {
+          var today = new Date(new Date().toJSON().slice(0, 10));
+          let diffTime = Math.abs(
+            today.getTime() - new Date(user.trial_start).getTime()
+          );
+          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+          user.trial_days =
+            user.trial_days - diffDays > 0 ? user.trial_days - diffDays : 0;
+          let dt1 = new Date(user.created);
+          let dt2 = new Date(user.updated);
+          user.appUsed = Math.floor(
+            (Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) -
+              Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) /
+              (1000 * 60 * 60 * 24)
+          );
+        });
+      },
+      (err) => {
+        this.loading = false;
+      }
+    );
   }
-
 
   pageLimit() {
     this.page.offset = 0;
-    localStorage.setItem('pageLimit', this.page.limit.toString());
+    localStorage.setItem("pageLimit", this.page.limit.toString());
     this.getCategoryData(this.page);
+  }
+
+  deleteCategory(e, category) {
+    if (e.target.checked) {
+      console.log("true");
+      this.deleteCatogaryArray.push(category._id);
+    } else {
+      console.log("falase");
+      var index = this.deleteCatogaryArray.indexOf(category._id);
+      this.deleteCatogaryArray.splice(index, 1);
+    }
+  }
+
+  saveCategoryList() {
+    let allProducts = [];
+    this.loading = true;
+    this.categoryData.forEach((catogary) => {
+      allProducts.push({
+        updateOne: {
+          filter: { _id: catogary._id },
+          update: { $set: catogary.document },
+        },
+      });
+    });
+
+    this.userService.categoryUpdateData({ data: allProducts }).subscribe(
+      (res) => {
+        this.loading = false;
+        this.getCategoryData(this.page);
+      },
+      (err) => {
+        this.loading = false;
+      }
+    );
+  }
+
+  deleteCategoryList() {
+    this.loading = true;
+    let allProducts = [];
+    console.log(this.deleteCatogaryArray);
+
+    this.deleteCatogaryArray.forEach((item) => {
+      allProducts.push({
+        deleteOne: {
+          filter: { _id: item },
+        },
+      });
+    });
+
+    this.userService.categoryUpdateData({ data: allProducts }).subscribe(
+      (res) => {
+        this.getCategoryData(this.page);
+        console.log(res);
+        this.loading = false;
+      },
+      (err) => {
+        this.loading = false;
+      }
+    );
+  }
+
+  updateData(e, category, key) {
+    var index = this.categoryData.indexOf(category);
+    console.log(e.target.value);
+    console.log(index);
+    this.categoryData[index]["document"][key] = e.target.value;
+    console.log(this.categoryData[index]);
+
+    // if (e.target.checked) {
+    //   console.log("true");
+    //   this.deleteCatogaryArray.push(category._id);
+    // } else {
+    //   console.log("falase");
+    //   this.deleteCatogaryArray.splice(index, 1);
+    // }
   }
 }
--- src/app/secure/user/user.service.ts
+++ src/app/secure/user/user.service.ts
@@ -1,39 +1,65 @@
-import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Http, Headers, Response } from '@angular/http';
-import { environment } from './../../../environments/environment';
-
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Http, Headers, Response } from "@angular/http";
+import { environment } from "./../../../environments/environment";
 
 @Injectable()
 export class UserService {
+  constructor(private http: Http) {}
 
-    constructor(private http: Http) { }
+  createAuthorizationHeader(headers: Headers) {
+    headers.append(
+      "Authorization",
+      localStorage.getItem("token").replace(/\"/g, "")
+    );
+  }
 
-    createAuthorizationHeader(headers: Headers) {
-        headers.append('Authorization', localStorage.getItem('token').replace(/\"/g, ""));
-    }
+  getUsers(page, limit) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http
+      .get(environment.apiUrl + "admin/user?page=" + page + "&limit=" + limit, {
+        headers: headers,
+      })
+      .pipe(map((response: any) => response.json()));
+  }
 
-    getUsers(page, limit) {
-        let headers = new Headers();
-        this.createAuthorizationHeader(headers);
-        return this.http.get(environment.apiUrl + 'admin/user?page='+page+'&limit='+limit, { headers: headers }).pipe(map((response: any) => response.json()));
-    }
+  getAccessToken(shopUrl) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http
+      .get(environment.apiUrl + "admin/access_token?shopUrl=" + shopUrl, {
+        headers: headers,
+      })
+      .pipe(map((response: any) => response.json()));
+  }
 
-    getAccessToken(shopUrl) {
-        let headers = new Headers();
-        this.createAuthorizationHeader(headers);
-        return this.http.get(environment.apiUrl + 'admin/access_token?shopUrl='+shopUrl, { headers: headers }).pipe(map((response: any) => response.json()));
-    }
+  getBrandData(page, limit) {
+    return this.http
+      .get("https://api.buy-indian.in/brand?page=" + page + "&limit=" + limit)
+      .pipe(map((response: any) => response.json()));
+  }
 
-    getBrandData(page , limit) {
-        return this.http.get('https://api.buy-indian.in/brand?page='+page+'&limit='+limit).pipe(map((response: any) => response.json()))
-    }
+  getCategoryData(page, limit) {
+    return this.http
+      .get(
+        "https://api.buy-indian.in/catogary?search=&page=" +
+          page +
+          "&limit=" +
+          limit
+      )
+      .pipe(map((response: any) => response.json()));
+  }
 
-    getCategoryData(page , limit) {
-        return this.http.get('https://api.buy-indian.in/catogary?search=&page='+page+'&limit='+limit).pipe(map((response: any) => response.json()))
-    }
+  updateData(data) {
+    return this.http
+      .post("https://api.buy-indian.in/update", data)
+      .pipe(map((response: any) => response.json()));
+  }
 
-    updateData(data) {
-        return this.http.post('https://api.buy-indian.in/update' , data).pipe(map((response: any) => response.json()))
-    }
+  categoryUpdateData(data) {
+    return this.http
+      .post("https://api.buy-indian.in/catogary/update", data)
+      .pipe(map((response: any) => response.json()));
+  }
 }
